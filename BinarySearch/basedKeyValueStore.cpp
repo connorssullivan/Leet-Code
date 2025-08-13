c3502bfe0d7554828fae291c0ef1b8bd379877f7
Connor Sullivan <connorssullivan25@gmail.com>
2025-08-13 10:48:53 -0400

    Started Linked list

---
+++ BinarySearch/basedKeyValueStore.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+
+
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(std::string key, std::string value, int timestamp) {
+        
+    }
+    
+    std::string get(std::string key, int timestamp) {
+        
+    }
+};
