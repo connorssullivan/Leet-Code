80d978593b6d36f578edeb23c15688eeb99a07c4
Connor Sullivan <connorssullivan25@gmail.com>
2025-09-23 14:11:56 -0400

    Backtracking 2

--- backTracking/subsets.cpp
+++ backTracking/subsets.cpp
@@ -1,25 +1,25 @@
 #include <vector>
 
 class Solution {
-    void dfs(std::vector<std::vector<int>>& sol, int index, const std::vector<int>& nums, std::vector<int>& current_set)
-    {
-        if (index >= nums.size())
-        {
-            sol.push_back(current_set);
-            return;
-        }
-
-        current_set.push_back(nums[index]);
-        dfs(sol, index+1, nums, current_set);
-        current_set.pop_back();
-        dfs(sol, index+1, nums, current_set);
-    }
 public:
     std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
         std::vector<std::vector<int>> sol;
         std::vector<int> curr_set;
 
-        dfs(sol, 0, nums, curr_set);
+        auto dfs = [&] (auto&& self, int index) {
+            if (index >= nums.size())
+            {
+                sol.push_back(curr_set);
+                return;
+            }
+
+            curr_set.push_back(nums[index]);
+            self(self, index+1);
+            curr_set.pop_back();
+            self(self, index+1);
+        };
+
+        dfs(dfs, 0);
 
         return sol;
     }
